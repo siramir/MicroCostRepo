f69cfb10423949464842df841167f27cca51a42c
siramir <a_marashy@yahoo.com>
2016-05-26 14:32:49 +1000

    unsucsessful "do" piecewise lm

--- apdcScript.R
+++ apdcScript.R
@@ -1,42 +1,33 @@
-################
-##    APDC   ###
-################
-
-# In this script I play with APDC data
-# gettin familiar with data
+# APDC data ---------------------------------------------------------------
 library(dplyr)
 library(lubridate)
 library(ggplot2)
+library(SiZer)
+library(gapminder)
+library(magrittr)
 
-# save(up45, file = "C:/Users/Amir/Documents/R/workingdir/Microsim/microsimulation/Data/up45Df.obj")
-
-setwd("C:/Users/Amir/Documents/R/workingdir/Microsim/microsimulation")
-load(paste(getwd(),'/Data/apdcDf.obj',sep = ''))
-
-# sample data
-# pbs_sample <- pbs_sim %>% sample_n(500)
+#load apdc file--------
+setwd("C:/Users/Amir/MicroCost")
+load(paste(getwd(),'/Data/apdc.obj',sep = ''))
 apdc_sample <- apdc %>% sample_n(10)
-# seef_sample <- seef_all45up_clean_02nov2011 %>% sample_n(500)
-# up45_sample <- up45 %>% sample_n(500)
 
-#some cleaning on apdc
-apdc$emergncy <- factor(apdc$emergncy)
-apdc$sex <- factor(apdc$sex)
-apdc$admdate <- as.Date(x = apdc$admdate,"%m/%d/%Y")
-apdc <- apdc%>% mutate(admYear = year(admdate))
+# some cleaning on apdc ---------------------------------------------------
 
-##--- Fill in the cost records with average cost for their drgs in costNew
-apdc <- mutate(apdc, costNew = cost_wt_a)
-apdc <- apdc[,c(1:41,176,42:175)]
+# apdc$emergncy <- factor(apdc$emergncy)
+# apdc$sex <- factor(apdc$sex)
+# apdc$admdate <- as.Date(x = apdc$admdate,"%m/%d/%Y")
+# apdc <- apdc%>% mutate(admYear = year(admdate))
 
-drgCostYear <-
-  apdc %>% select(ardrg,admdate, admYear, cost_wt_a,cost_wt_d, los)
+# Data based on date ------------------------------------------------------
 
-summArdrg <-
-  drgCostYear %>% group_by(ardrg,admYear) %>% summarise(avg = mean(cost_wt_a,na.rm = TRUE),
-                                                        varA = var(cost_wt_a,na.rm = TRUE), varD = var(cost_wt_d,na.rm = TRUE), min =
-                                                          min(cost_wt_d,na.rm = TRUE),max = max(cost_wt_d,na.rm = TRUE),n=n()
-  )
+# beginDate <- apdc$admdate[1] 
+# endDate <- beginDate + years(1)
+# costYear <- interval(beginDate, endDate) 
+# selected <- apdc %>% filter(admdate %within% costYear)
+
+# Plot cost_wt_a vs los for each drg --------------------------------------
+drgCostYear <- apdc %>% select(ardrg,admdate, admYear, cost_wt_a,cost_wt_avg, los)
+summArdrg <- drgCostYear %>% group_by(ardrg,admYear) %>% summarise(avg = mean(cost_wt_a,na.rm = TRUE),n =n())
 
 bzDrg <-  summArdrg %>% filter( n >100) 
 drgList <- unique(bzDrg$ardrg)
@@ -47,70 +38,73 @@ dir.create(savePath)
 randomIndx <- sample(1:length(drgList), 1, replace = FALSE)
 drg <-  drgList[randomIndx]
 drgData <- drgCostYear %>% filter(ardrg == drg)
-
-jpeg(paste(savePath,'losWeight_',drg,'.jpg',sep=''),width = 4, height = 4, units = 'in', res = 300)
+jpeg(paste(savePath,drg,'_losWeight_avg.jpg',sep=''),width = 4, height = 4, units = 'in', res = 300)
+qplot(
+  x = los, y = jitter(cost_wt_avg), data = drgData, color = factor(admYear), main = drg,na.rm = TRUE
+)
+dev.off()
+jpeg(paste(savePath,drg,'_losWeight_a.jpg',sep=''),width = 4, height = 4, units = 'in', res = 300)
 qplot(
   x = los, y = jitter(cost_wt_a), data = drgData, color = factor(admYear), main = drg,na.rm = TRUE
 )
 dev.off()
 
+# Fill in the cost records with average cost for their drgs in cos --------
 
-naIndx <- which(is.na(drgCost$cost_wt_a))
-naCost <- drgCost[naIndx,]
-joined <- left_join(naCost,summArdrg, by = 'ardrg')
-apdc[naIndx,"costNew"] <- joined$avg
-rm(joined,naCost,naIndx,drgCost,summArdrg)
+apdc <- mutate(apdc, cost_wt_avg = cost_wt_a)
+drgCostYear <- apdc %>% select(ardrg,admdate, admYear, cost_wt_a,cost_wt_d, los)
+summArdrg <- drgCostYear %>% group_by(ardrg,admYear) %>% summarise(avg = mean(cost_wt_a,na.rm = TRUE),n =n())
 
-indiv <- apdc %>% group_by(PPN)  %>% summarise(costWeight = sum(costNew,na.rm = TRUE),n = n())
-summary(indiv)
+# Phase one DRG Year mean
+naIndx <- which(is.na(drgCostYear$cost_wt_a))
+naCost <- drgCostYear[naIndx,]
+joined <- left_join(naCost,summArdrg, by = c('ardrg','admYear'))
+apdc[naIndx,"cost_wt_avg"] <- joined$avg
 
-apdc <- apdc%>% mutate(year = year(admdate))
+# Phase two DRG mean
+naIndx <- which(is.na(drgCostYear$cost_wt_a))
+naCost <- drgCostYear[naIndx,]
+summArdrg <- drgCostYear %>% group_by(ardrg) %>% summarise(avg = mean(cost_wt_a,na.rm = TRUE),n =n())
+joined <- left_join(naCost,summArdrg, by ='ardrg')
+apdc[naIndx,"cost_wt_avg"] <- joined$avg
+rm(joined,naCost,naIndx,drgCostYear,summArdrg)
 
+# Draft -------------------------------------------------------------------
+# # number of records without cost = 466001 # naCost <-
+# sum(is.na(apdc$cost_wt_a))
 
-# Data extraction based on admission date
-beginDate <- apdc$admdate[1]
-endDate <- beginDate + years(1)
-costYear <- interval(beginDate, endDate)
-selected <- apdc %>% filter(admdate %within% costYear)
-
-
-## Draft--------------
-byPpn <- apdc %>% group_by(PPN)
-asad <- byPpn %>% summarize(beginDate =  min(admdate))
-asad <- asad %>% mutate(endDate = asad$beginDate + years(1),costYear = interval(beginDate, endDate))
-apdc <- apdc %>% left_join(asad[,c('PPN','costYear')])
-# selected <- apply(apdc, function(y) {filter(y$admdate %within% y$costYear)
-
-# plotWeight <- function(drgCostYear){
-#   p <- qplot(data=drgCostYear,
-#              x=los,
-#              y=cost_wt_a)
-#   print(p)
-#   
-# }
-# 
-# group_by(drgCostYear, ardrg) %>%
-#   do(plot = plotWeight(.))
-# # colnames(apdc)
-# # hist(summArdrg$avg ,breaks = 100)
-# #
-#
-
-# akbar <- mutate(akbar, month = month(apdc$admdate))
-# byYearMonth <- group_by(akbar, year,month)
-# sum <- summarise(byYearMonth, mean(los),n())
-# qplot(y = sum$`n()`,data = sum, color = factor(sum$month))
-
-
-#read sas file
-# read_sas(b7dat = 'C:\\Users\\Amir\\Desktop\\micro\\DATA\\Synthetic MBS and PBS data\\Synthetic MBS\\mbs_sim_v3.sas7bdat' )
-
-# number of records without cost = 466001
-# naCost <- sum(is.na(apdc$cost_wt_a))
-
-
-# attach(apdc)  
-# a <- ggplot(apdc,aes(admdate,color=apdc$year))
-# a + geom_bar(position = "fill")
+# # attach(apdc) 
+# a <- ggplot(apdc,aes(admdate,color=apdc$year)) 
+# a + # geom_bar(position = "fill") 
 # qplot(y = admdate, color = years(admdate))
 
+
+# indiv <- apdc %>% group_by(PPN)  %>% summarise(costWeight = sum(costNew,na.rm = TRUE),n = n())
+byPpn <- apdc %>% group_by(PPN) asad <- byPpn %>% summarize(beginDate =  min(admdate)) 
+asad <- asad %>% mutate(endDate = asad$beginDate + years(1),costYear = interval(beginDate, endDate)) 
+apdc <- apdc %>% left_join(asad[,c('PPN','costYear')])
+savePath <- paste(getwd(),'/Data/',sep='')
+dir.create(savePath)
+save(apdc, file = paste(savePath,"apdc.obj",sep = ""))
+
+plm <- function (df) {
+  x <- df %>% select(los)
+  x <- as.vector(x)
+  x <- unlist(x)
+  x <- unname(x)
+  y <- df %>% select(cost_wt_a)
+  y <- as.vector(y)
+  y <- unlist(y)
+  y <- unname(y)
+  model <- piecewise.linear(x = x,y = y,CI=FALSE)
+  return(model)
+}
+
+plot(model)
+print(model)
+predict(model, 2001)
+
+models <-
+  apdc %>% filter(!is.na(cost_wt_a)) %>% 
+  select(ardrg, admYear,los,cost_wt_a) %>% 
+  group_by(ardrg,admYear) %>% do(model = plm(.))
